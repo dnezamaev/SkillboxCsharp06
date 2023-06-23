6a2fe60a5fa96c59d8ae22147c05305f64578b06
Nezamaev Dmitriy <dnezamaev@gmail.com>
2023-06-23 21:57:17 +0300

    Add project files.

---
+++ SkillboxCsharp06/CommonMistakes.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillboxCsharp06
+{
+    internal class AbsolutePath
+    {
+        /// <summary>
+        /// Ошибка. Использование абсолютного пути к файлу.
+        /// На системе может быть запрещена запись в указанную директорию.
+        /// Может не быть диска с: (например, на Linux).
+        /// Может не существовать такой директории.
+        /// Пользователям не нравится, когда программа создает файлы где попало.
+        /// </summary>
+        void Mistake()
+        {
+            using (StreamWriter writer = new StreamWriter(@"C:\MyCoolFile.txt")) { }
+        }
+
+        /// <summary>
+        /// Решение.
+        /// Использовать относительные пути. 
+        /// Для этого достаточно указать просто имя файла без буквы диска.
+        /// </summary>
+        void Solution()
+        {
+            using (StreamWriter writer = new StreamWriter("MyCoolFile.txt")) { }
+        }
+    }
+
+    class DuplicateFilePath
+    {
+        /// <summary>
+        /// Ошибка. Дублирование пути к файлу.
+        /// Если потребуется изменить имя файла, 
+        /// нужно будет бегать по всему коду и менять его в нескольких местах.
+        /// </summary>
+        void Mistake()
+        {
+            using (StreamReader reader = new StreamReader("MyCoolFile.txt")) { }
+
+            using (StreamWriter writer = new StreamWriter("MyCoolFile.txt")) { }
+
+            File.ReadAllLines("MyCoolFile.txt");
+        }
+
+        /// <summary>
+        /// Решение.
+        /// Завести переменную или константу для пути.
+        /// Передавать её в качестве параметра методам по необходимости.
+        /// </summary>
+        /// <param name="filePath"></param>
+        void Solution(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath)) { }
+
+            using (StreamWriter writer = new StreamWriter(filePath)) { }
+
+            File.ReadAllLines(filePath);
+        }
+    }
+
+    class FileCreateWithoutClose
+    {
+        /// <summary>
+        /// Ошибка. Использование File.Create без закрытия потока.
+        /// Этот метод создает файл и возращает поток для работы с ним.
+        /// Если его не закрыть, то повторное открытие файла может вызвать ошибку.
+        /// </summary>
+        void Mistake(string DataFilePath)
+        {
+            File.Create(DataFilePath);
+        }
+
+        /// <summary>
+        /// Решение.
+        /// Закрывать поток сразу же после создания файла.
+        /// </summary>
+        void Solution1(string DataFilePath)
+        {
+            File.Create(DataFilePath).Close();
+        }
+
+        /// <summary>
+        /// Решение.
+        /// Сохранять поток в переменной для дальнейшго использования и
+        /// закрывать его через using.
+        /// </summary>
+        void Solution2(string DataFilePath)
+        {
+            using (FileStream stream = File.Create(DataFilePath))
+            {
+                // Работа с потоком stream.
+            }
+        }
+    }
+}
---
+++ SkillboxCsharp06/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+
+namespace SkillboxCsharp06
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Путь к файлу с данными.
+        /// </summary>
+        const string DataFilePath = "workers.txt";
+
+        /// <summary>
+        /// Название "столбцов" в структуре файла.
+        /// </summary>
+        static string[] DataColumnNames =
+        {
+            "ID", "Дата добавления", "Ф.И.О.", "Возраст",
+            "Рост", "Дата рождения", "Место рождения"
+        };
+
+        /// <summary>
+        /// Разделитель, используемый в файле.
+        /// </summary>
+        static string FileColumnSeparator = "#";
+
+        static void Main(string[] args)
+        {
+            // Проверяем наличие файла и выводим меню.
+            CheckFileExists();
+            Menu();
+        }
+
+        /// <summary>
+        /// Проверяет наличие файла и создает при необходимости.
+        /// </summary>
+        static void CheckFileExists()
+        {
+            // Проверяем наличие файла.
+            if (!File.Exists(DataFilePath))
+            {
+                // Создаем файл и сразу же закрываем поток.
+                File.Create(DataFilePath).Close();
+                Console.WriteLine("Файл создан");
+            }
+
+        }
+        
+        /// <summary>
+        /// Выводит меню и разбирает вывод пользователя.
+        /// Передаёт обработку задачи в другие методы.
+        /// </summary>
+        static void Menu()
+        {
+            // Бесконечный цикл вывода меню.
+            while (true)
+            {
+                Console.WriteLine("1 - вывести список сотрудников на экран");
+                Console.WriteLine("2 - добавить нового сотрудника");
+                Console.WriteLine("0 - выход");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "0": // Выход.
+                        return;
+                    case "1": // Вывод списка сотрудников.
+                        PrintAllWorkers();
+                        break;
+                    case "2": // Добавление сотрудника.
+                        AddWorkerToFile();
+                        break;
+                    default: // Обработка несуществующего пункта меню.
+                        Console.WriteLine("Неизвестный пункт меню");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Запрашивает у пользователя информацию о сотруднике и
+        /// записывает её в файл.
+        /// </summary>
+        static void AddWorkerToFile()
+        {
+            // Массив данных о новом сотрудники.
+            // Размер массива совпадает с количеством столбцов в структуре файла.
+            string[] dataItems = new string[DataColumnNames.Length];
+
+            // Пробегаемся по каждому элементу структуры.
+            for (int i = 0; i < dataItems.Length; i++)
+            {
+                // Дату добавления вводить не нужно.
+                if (i == 1)
+                {
+                    // Она заполняется автоматически.
+                    dataItems[i] = DateTime.Now.ToString();
+                    continue;
+                }
+
+                // Выводим приглашение для пользователя и сохраняем введённое
+                // в нужный элемент массива.
+                Console.Write($"Введите {DataColumnNames[i]}: ");
+                dataItems[i] = Console.ReadLine();
+            }
+
+            // Склеиваем полученные значения через разделитель.
+            string totalWorkerInfo = string.Join(FileColumnSeparator, dataItems);
+
+            // Создаем записывающий поток для файла.
+            // Второй параметр означает добавление в конец файла, а не перезапись.
+            // using гарантирует закрытие потока.
+            using (StreamWriter writer = new StreamWriter(DataFilePath, true))
+            {
+                writer.WriteLine(totalWorkerInfo);
+            }
+        }
+
+        /// <summary>
+        /// Выводит список сотрудник из файла на экран.
+        /// </summary>
+        static void PrintAllWorkers()
+        {
+            // Создаем читающий поток для файла.
+            // using гарантирует закрытие потока.
+            using (StreamReader reader = new StreamReader(DataFilePath))
+            {
+                // Цикл чтения до конца потока.
+                while (!reader.EndOfStream)
+                {
+                    // Читаем очередную строку из файла.
+                    string line = reader.ReadLine();
+
+                    // Разбиваем на элементы.
+                    string[] dataItems = line.Split(FileColumnSeparator);
+
+                    // Выводим информацию о сотруднике.
+                    for (int i = 0; i < dataItems.Length; i++)
+                    {
+                        Console.WriteLine(
+                            $"{DataColumnNames[i]}: {dataItems[i]}"); 
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+}
